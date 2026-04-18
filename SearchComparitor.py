d5809414440910fbb8be8722f27a98fb0c14024b
saivignesh1437 <maruboina.saivignesh@klh.edu.in>
2026-04-18 22:41:47 +0530

    Added Binary Search

--- SearchComparitor.py
+++ SearchComparitor.py
@@ -1,9 +1,26 @@
 import streamlit as st
 
+#Algorithms
 def linear_search(arr, key):
     for i in range(len(arr)):
         if arr[i] == key:
             return i
     return -1
+
+def binary_search(arr, key):
+    low = 0
+    high = 0 
+    len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
 
 st.title("Algorithm Comparator")
