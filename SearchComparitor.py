c5fa4c0eb64268405acfd87610dc7471f2df1b70
saivignesh1437 <maruboina.saivignesh@klh.edu.in>
2026-04-18 22:39:21 +0530

    Added Linear Search

--- SearchComparitor.py
+++ SearchComparitor.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+def linear_search(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+
 st.title("Algorithm Comparator")
