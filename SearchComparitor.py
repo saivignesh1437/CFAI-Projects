b4e1014dfb45be180764ec07d8490a29db3d5c0d
saivignesh1437 <maruboina.saivignesh@klh.edu.in>
2026-04-18 22:46:46 +0530

    Calculation of time taken for both Search methods

--- SearchComparitor.py
+++ SearchComparitor.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-#Algorithms
+# Algorithms
 def linear_search(arr, key):
     for i in range(len(arr)):
         if arr[i] == key:
@@ -27,3 +27,16 @@ st.title("Algorithm Comparator")
 
 size = st.slider("Input Size", 100, 100000, 1000)
 target = st.number_input("Enter number to search", min_value=0, max_value=size-1, value=0)
+
+if st.button("Compare Algorithms"):
+    arr = list(range(size))  # already sorted
+
+    # Linear Search
+    start = time.time()
+    linear_result = linear_search(arr, target)
+    linear_time = time.time() - start
+
+    # Binary Search
+    start = time.time()
+    binary_result = binary_search(arr, target)
+    binary_time = time.time() - start
