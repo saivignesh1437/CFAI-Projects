7c444a267ab7a8edb84de960defa372d13a52fd8
saivignesh1437 <maruboina.saivignesh@klh.edu.in>
2026-04-18 22:56:12 +0530

    Showing the Fastest Algorithm

--- SearchComparitor.py
+++ SearchComparitor.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 # Algorithms
 def linear_search(arr, key):
@@ -9,8 +10,7 @@ def linear_search(arr, key):
 
 def binary_search(arr, key):
     low = 0
-    high = 0 
-    len(arr) - 1
+    high = len(arr) - 1
     
     while low <= high:
         mid = (low + high) // 2
@@ -22,26 +22,32 @@ def binary_search(arr, key):
             high = mid - 1
     return -1
 
-
-st.title("Algorithm Comparator")
+# UI
+st.title("Time Comparison: Linear vs Binary Search")
 
 size = st.slider("Input Size", 100, 100000, 1000)
-target = st.number_input("Enter number to search", min_value=0, max_value=size-1, value=0)
+key = st.number_input("Enter number to search", min_value=0, max_value=size-1, value=0)
 
 if st.button("Compare Algorithms"):
     arr = list(range(size))  
 
     # Linear Search
     start = time.time()
-    linear_result = linear_search(arr, target)
+    linear_result = linear_search(arr, key)
     linear_time = time.time() - start
 
     # Binary Search
     start = time.time()
-    binary_result = binary_search(arr, target)
+    binary_result = binary_search(arr, key)
     binary_time = time.time() - start
 
     # Displaying Results
     st.subheader("Time Comparison")
     st.write(f"Linear Search Time: {linear_time:.8f} seconds")
     st.write(f"Binary Search Time: {binary_time:.8f} seconds")
+
+    #  Faster algorithm
+    if linear_time < binary_time:
+        st.success("Linear Search is faster (unexpected for large input!)")
+    else:
+        st.success("Binary Search is faster ")
